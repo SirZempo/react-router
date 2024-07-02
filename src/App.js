97415940a4c44a195103f7a8203bc4f8e27231bd
SirZempo <uaemexzempo@gmail.com>
2024-07-02 17:29:40 -0600

    ✨Se creo Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
+import './assets/css/componentes/card.css';
+import Home from "./pages/Home.jsx";
+import Sobre from "./pages/Sobre.jsx";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/sobre" element={<Sobre></Sobre>}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
