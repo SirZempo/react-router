4980df11c4db9a3660324bb86eebc906fb242ea3
SirZempo <uaemexzempo@gmail.com>
2024-07-03 12:36:23 -0600

    ⚡ Exhibiendo Post y categorias

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home.jsx";
 import Sobre from "./pages/Sobre.jsx";
 import Page404 from './pages/Page404';
 import Header from "./components/Header.jsx";
+import Post from './pages/Post';
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
@@ -17,6 +18,7 @@ function App() {
           { /*Todos los componentes dentro de Routes es dinamico */}
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/sobre" element={<Sobre></Sobre>}></Route>
+          <Route path="/posts/:id" element={<Post></Post>}></Route>
           <Route path="*" element={<Page404></Page404>}></Route>
         </Routes>
       </Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import image from "../assets/img/doguito.svg";
 import "../assets/css/componentes/header.css";
+import { Link } from "react-router-dom";
 
 const Header = () =>{
     return (
@@ -8,17 +9,17 @@ const Header = () =>{
                 <span className="menu-hamburguer__icon"></span>
             </div>
             <div className="header-container">
-                <a href="/" className="flex flex--center">
+                <Link to="/" className="flex flex--center">
                     <img src={image} alt="doguito" className="header__logo" />
                     <h1 className="header__title">Petshop</h1>
-                </a>
+                </Link>
             </div>
             <nav className="menu-header">
                 <ul className="menu-items">
-                    <li><a href="#" className="menu-item menu-item--entrar">Entrar</a></li>
-                    <li><a href="#" className="menu-item">Productos</a></li>
-                    <li><a href="/" className="menu-item">Blog</a></li>
-                    <li><a href="/sobre" className="menu-item">Sobre</a></li>
+                    <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+                    <li><Link to="#" className="menu-item">Productos</Link></li>
+                    <li><Link to="/" className="menu-item">Blog</Link></li>
+                    <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
                 </ul>
             </nav>
             <div className="menu-header-background"></div>
